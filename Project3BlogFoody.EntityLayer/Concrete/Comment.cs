d584f15c3efa4549adf11e0cb9e7af4a2a9ca300
Fatih Kürekçi <fatihkr02@gmail.com>
2024-11-05 22:49:00 +0300

    Add project files.

---
+++ Project3BlogFoody.EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3BlogFoody.EntityLayer.Concrete
+{
+    public class Comment
+    {
+        public int MyProperty { get; set; }
+    }
+}
